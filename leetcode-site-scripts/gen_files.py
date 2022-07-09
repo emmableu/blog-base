2086c2499b4ade8681fe59eaf8e9c1a7ad4fc7e6
wwang33 <wwang33@ncsu.edu>
2022-07-08 22:35:50 -0700

    updated

--- leetcode-site-scripts/gen_files.py
+++ leetcode-site-scripts/gen_files.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 from collections import defaultdict
 
-df = pd.read_csv("/Users/wwang33/Documents/vuepress/blog-base/scripts/leetcode calendar - google.csv")
+df = pd.read_csv("./leetcode calendar - google.csv")
 print(df.head())
 
 for i in df.index:
@@ -10,9 +10,9 @@ for i in df.index:
     tags = tag_str.split("[DELIM]")
     tags_name = ", ".join(tags)
     title = df.at[i, 'title'].split("[DELIM]")[0].replace("?", "")
-    file_name = f"{df.at[i, 'frequency']}.{df.at[i, 'number']} - {title} - {df.at[i, 'difficulty']}.md"
+    file_name = f"{'{:03}'.format(df.at[i, 'frequency'])}.{df.at[i, 'number']} - {title} - {df.at[i, 'difficulty']}.md"
 
-    f = open(f"/Users/wwang33/Documents/vuepress/blog-base/docs/06.Google/03.Incomplete/{file_name}", "w")
+    f = open(f"/Users/wwang33/Documents/vuepress/Google - 03.Incomplete/{file_name}", "w")
     f.write(f"tags: [{tags_name}]")
     f.close()
 
