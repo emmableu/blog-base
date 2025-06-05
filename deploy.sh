ea667d0b17b7d8c77ec3eff31c7bfdc7de76be10
Wengran Wang <wengranwang@Wengrans-MacBook-Air.local>
2025-06-04 17:28:25 -0700

    updated

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,10 @@ git commit -m 'updated'
 git push origin master
 
 
-push_addr=git@github.com:emmableu/blog.git # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+push_addr=https://github.com/emmableu/blog.git # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
 commit_info=`git describe --all --always --long`
 dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
-push_branch=gh-pages # 推送的分支
+push_branch=master # 推送的分支
 
 # 生成静态文件
 npm run build
