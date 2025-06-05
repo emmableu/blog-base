fb7c4741e6c13152af756749f10885369bde7a76
Wengran Wang <wengranwang@Wengrans-MacBook-Air.local>
2025-06-04 17:33:22 -0700

    updated

--- docs/login/Login.vue
+++ docs/login/Login.vue
@@ -18,7 +18,7 @@
 </template>
 
 <script>
-import {checkAuth, STORAGE_KEY} from './helper'
+import { checkAuth, STORAGE_KEY } from './helper'
 
 export default {
   data () {
@@ -34,20 +34,16 @@ export default {
           name: this.username,
           password: this.password,
         })
-        // 登录成功后的逻辑处理，这里将数据保存在 localStorage 中仅作为功能演示
+        // Save data to localStorage as a demonstration
         window.localStorage.setItem(STORAGE_KEY, data)
+
         if (checkAuth()) {
           this.$emit('close', true)
-        }
-        else {
-          this.$dlg.alert('用户名密码错误！', {
-            messageType: 'warning'
-          })
+        } else {
+          alert('用户名密码错误！')
         }
       } else {
-        this.$dlg.alert('Please complete the content', {
-          messageType: 'warning'
-        })
+        alert('Please complete the content')
       }
     }
   }
@@ -55,33 +51,30 @@ export default {
 </script>
 
 <style lang="stylus">
-.v-dialog-overlay
-  opacity: 1 !important
-  background-color: darkgrey !important
 .login-form
   padding: 1rem
-  display flex
-  flex-direction column
-  box-sizing border-box
+  display: flex
+  flex-direction: column
+  box-sizing: border-box
   .btn-row
-    margin-top 1rem
+    margin-top: 1rem
   .btn
-    padding 0.6rem 2rem
-    outline none
-    background-color #60C084
-    color white
-    border 0
+    padding: 0.6rem 2rem
+    outline: none
+    background-color: #60C084
+    color: white
+    border: 0
   .form-header
-    color #666
-    margin-bottom 0.5rem
+    color: #666
+    margin-bottom: 0.5rem
   .form-control
-    padding 0.6rem
-    border 2px solid #ddd
-    width 100%
-    margin-bottom 0.5rem
-    box-sizing border-box
-    outline none
-    transition border 0.2s ease
+    padding: 0.6rem
+    border: 2px solid #ddd
+    width: 100%
+    margin-bottom: 0.5rem
+    box-sizing: border-box
+    outline: none
+    transition: border 0.2s ease
     &:focus
-      border 2px solid #aaa
+      border: 2px solid #aaa
 </style>
